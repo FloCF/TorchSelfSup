2ac0e0ae79d27f41dcac2c448e3f08dcc750f9c0
Florian C.F. Schulz <florian.cf.schulz@tu-berlin.de>
2022-01-21 12:21:46 +0100

    Add DirectCLR to repo

--- models/simclr.py
+++ models/simclr.py
@@ -13,9 +13,12 @@ from .utils import MLP
 class SimCLR(nn.Module):
     def __init__(self, backbone_net: nn.Module,
                  projector_hidden: Union[int, tuple] = (2048, 2048, 256),
-                 temperature: float = 0.2):
+                 temperature: float = 0.2,
+                 direct_dim: Optional[int] = None):
         super().__init__()
         
+        # DirectCLR appoach from https://arxiv.org/abs/2110.09348 (default 360 for ResNet50)
+        self.direct_dim = int(direct_dim)
         self.nt_xent_loss = NTXentLoss(temperature)
         
         self.backbone_net = backbone_net
@@ -32,6 +35,9 @@ class SimCLR(nn.Module):
         # Encoder
         p1, p2 = self.backbone_net(x1), self.backbone_net(x2)
         # Projector
-        p1, p2 = self.projector(p1), self.projector(p2)
+        if self.direct_dim:
+            p1, p2 = p1[:self.direct_dim], p2[:self.direct_dim]
+        else:
+            p1, p2 = self.projector(p1), self.projector(p2)
             
         return self.nt_xent_loss(p1, p2)
